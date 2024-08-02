6efef61926b4590de5074e53fd112b8b1f5c9fd7
Mohd Zafar <mohdzafar97597@gmail.com>
2024-08-02 12:28:41 +0530

    set user profile

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -4,5 +4,5 @@ import userProfile from "../../public/assets/images/avatars/zafar.jpg"
 export const account = {
   displayName: 'mohd zafar',
   email: 'mohdzafar97597@gmail.com',
-  photoURL: {userProfile},
+  photoURL: userProfile,
 };
