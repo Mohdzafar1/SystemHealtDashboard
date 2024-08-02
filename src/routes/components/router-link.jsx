d3fe34c6309f6c02dd9ff9b4bb255fc9d6f31d38
Mohd Zafar <mohdzafar97597@gmail.com>
2024-08-02 10:59:23 +0530

    few update in packages

--- src/routes/components/router-link.jsx
+++ src/routes/components/router-link.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import React,{ forwardRef } from 'react';
+import React, { forwardRef } from 'react';
+
 // ----------------------------------------------------------------------
 
 const RouterLink = forwardRef(({ href, ...other }, ref) => <Link ref={ref} to={href} {...other} />);
+
+RouterLink.displayName = 'RouterLink';
 
 RouterLink.propTypes = {
   href: PropTypes.string,
